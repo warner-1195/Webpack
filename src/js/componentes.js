7a73333bc9b02c2c9611c5123d3673aca91ced02
Warner Sanchez <chocobarber0895@gmail.com>
2022-10-22 16:40:59 -0600

    Webpack

---
+++ src/js/componentes.js
@@ -0,0 +1,19 @@
+import '../css/componente.css';
+import webpacklogo from '../assets/img/webpack-logo.png'
+
+
+
+export const saludar = (nombre) => {
+
+    console.log('creando etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Hola, ${nombre}`;
+
+    document.body.append(h1);
+
+    //img
+    const img = document.createElement('img');
+    img.src = webpacklogo;
+    document.body.append(img);
+
+}
